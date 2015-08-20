c592415507c0d27ce9162e0d9f3b81dcac5c64a7
Alexander Batishchev <abatishchev@gmail.com>
2015-08-20 14:11:34 -0700

    Adding api client

---
+++ NicheLens.Scrapper.Api.Client/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NicheLens.Scrapper.Api.Client")]
+[assembly: AssemblyProduct("NicheLens.Scrapper.Api.Client")]
+
+[assembly: Guid("89697ae0-49af-45e1-a347-b15f47be2a5e")]
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
---
+++ NicheLens.Scrapper.Api.Client/ScrapperApi/ScrapperApiAppServiceExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.Azure.AppService;
+
+namespace NicheLens.Scrapper.Api.Client
+{
+    public static class ScrapperApiAppServiceExtensions
+    {
+        public static ScrapperApi CreateScrapperApi(this IAppServiceClient client)
+        {
+            return new ScrapperApi(client.CreateHandler());
+        }
+
+        public static ScrapperApi CreateScrapperApi(this IAppServiceClient client, params DelegatingHandler[] handlers)
+        {
+            return new ScrapperApi(client.CreateHandler(handlers));
+        }
+
+        public static ScrapperApi CreateScrapperApi(this IAppServiceClient client, Uri uri, params DelegatingHandler[] handlers)
+        {
+            return new ScrapperApi(uri, client.CreateHandler(handlers));
+        }
+
+        public static ScrapperApi CreateScrapperApi(this IAppServiceClient client, HttpClientHandler rootHandler, params DelegatingHandler[] handlers)
+        {
+            return new ScrapperApi(rootHandler, client.CreateHandler(handlers));
+        }
+    }
+}
