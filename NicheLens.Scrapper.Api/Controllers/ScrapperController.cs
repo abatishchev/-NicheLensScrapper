a3572d69891a6dbc07e8c4b7cb4527505fca5fd2
Alexander Batishchev <abatishchev@gmail.com>
2015-08-20 12:40:11 -0700

    Adding ScrapperController stub

---
+++ NicheLens.Scrapper.Api/Controllers/ScrapperController.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Web.Http;
+
+namespace NicheLens.Scrapper.Api.Controllers
+{
+	public class ScrapperController : ApiController
+	{
+		[Route("api/scrapper/start")]
+		public IHttpActionResult Get()
+		{
+			return Ok(MethodBase.GetCurrentMethod().GetCustomAttribute<RouteAttribute>().Template);
+		}
+	}
+}
