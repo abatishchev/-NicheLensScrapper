5e2a06518d50c5ce67652c5eecfbfe7454e7cc1d
Alexander Batishchev <abatishchev@gmail.com>
2015-08-21 12:22:28 -0700

    Adding WebJobs.Tests

---
+++ NicheLens.Scrapper.WebJobs.Tests/ContainerConfigTest.cs
@@ -0,0 +1,18 @@
+﻿using SimpleInjector;
+using Xunit;
+
+namespace NicheLens.Scrapper.WebJobs.Tests
+{
+	public class ContainerConfigTest
+	{
+		[Fact]
+		public void Container_Verify_Should_Not_Throw_Exception()
+		{
+			// Arrange
+			var container = ContainerConfig.CreateContainer();
+
+			// Act
+			container.Verify(VerificationOption.VerifyOnly);
+		}
+	}
+}
---
+++ NicheLens.Scrapper.WebJobs.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NicheLens.Scrapper.WebJobs.Tests")]
+[assembly: AssemblyProduct("NicheLens.Scrapper.WebJobs.Tests")]
+
+[assembly: Guid("6da9de86-e6f9-4081-b556-fc1159b6a7a4")]
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
