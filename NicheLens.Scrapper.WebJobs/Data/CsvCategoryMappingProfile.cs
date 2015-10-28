60339c6204c74683f831cdcb9f00a56eb12a1d44
Alexander Batishchev <abatishchev@gmail.com>
2015-10-28 16:00:41 -0700

    Updating nuget packages

--- NicheLens.Scrapper.WebJobs/Data/CsvCategoryMappingProfile.cs
+++ NicheLens.Scrapper.WebJobs/Data/CsvCategoryMappingProfile.cs
@@ -9,10 +9,5 @@ namespace NicheLens.Scrapper.WebJobs.Data
 		{
 			CreateMap<CsvCategory, Category>().ForMember(d => d.Name, o => o.MapFrom(x => x.Title));
 		}
-
-		public override string ProfileName
-		{
-			get { return typeof(CsvCategoryMappingProfile).Name; }
-		}
 	}
 }
