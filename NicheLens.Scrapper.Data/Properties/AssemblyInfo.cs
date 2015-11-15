1e514fcb758ccdbe938e051145fd5df229301fb5
Alexander Batishchev <abatishchev@gmail.com>
2015-11-15 12:53:33 -0800

    Adding empty Data project

---
+++ NicheLens.Scrapper.Data/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NicheLens.Scrapper.Data")]
+[assembly: AssemblyProduct("NicheLens.Scrapper.Data")]
+
+[assembly: Guid("1801204c-33fb-4a5c-8bd9-d9fa5746a6d7")]
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
