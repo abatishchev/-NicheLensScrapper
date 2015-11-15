1217414f46b46f838235f2859e0a849c65a9da41
Alexander Batishchev <abatishchev@gmail.com>
2015-11-15 10:27:00 -0800

    Renaming to Data.Scheme. Adding AssemblyInfo

---
+++ NicheLens.Scrapper.Data.Scheme/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NicheLens.Scrapper.Data.Scheme")]
+[assembly: AssemblyProduct("NicheLens.Scrapper.Data.Scheme")]
+
+[assembly: Guid("d62e446c-2dbe-499c-992c-f12bbd7cf3a9")]
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
