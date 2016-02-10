2a4cc1ec78f1e7915913b4c194487479c7cc02af
Alexander Batishchev <abatishchev@gmail.com>
2016-02-09 23:42:13 -0800

    Removing FK between categories and products

--- NicheLens.Scrapper.Data.Scheme/Schemas/dbo/Tables/Keys/Foreign/FK_Product_CategoryId_Category.sql
+++
@@ -1,9 +0,0 @@
-﻿alter table dbo.Product
-	add constraint FK_Product_CategoryId_Category
-	foreign key (CategoryId)
-	references dbo.Category (CategoryId)
-go
-
-alter table dbo.Product
-	check constraint FK_Product_CategoryId_Category
-go
