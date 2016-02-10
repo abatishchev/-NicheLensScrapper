a168c9cb192f5d2333d5b0507905cae55e779fbe
Alexander Batishchev <abatishchev@gmail.com>
2016-02-09 23:42:34 -0800

    Limit category name to 255

--- NicheLens.Scrapper.Data.Scheme/Schemas/dbo/Tables/Category.sql
+++ NicheLens.Scrapper.Data.Scheme/Schemas/dbo/Tables/Category.sql
@@ -1,7 +1,7 @@
 ﻿create table dbo.Category
 (
 	CategoryId UniqueIdentifier not null,
-	Name VarChar(50) not null,
+	Name VarChar(255) not null,
 	NodeId BigInt not null,
 	ParentNodeId BigInt not null,
 	SearchIndex VarChar(50) not null
